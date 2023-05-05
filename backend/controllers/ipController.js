df0d75f019d463532f1b9a1c2781d35e1827691d
mohit <mohitpardeshi729@gmail.com>
2023-05-05 20:49:03 +0530

    returns full chain of IP addresses

--- backend/controllers/ipController.js
+++ backend/controllers/ipController.js
@@ -1,7 +1,8 @@
 function getIP(req, res) {
   const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const fullIP = ipAddress;
   const ip = ipAddress.split(',')[0];
-  return res.json({ ip });
+  return res.json({ ip , fullIP });
 }
 
 module.exports = { getIP };
